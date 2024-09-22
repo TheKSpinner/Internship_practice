4fa63c7c45fb9a9ff27eded13fc0232391edb377
TheKSpinner <alexandrusosu@yahoo.com>
2024-09-22 23:08:11 +0300

    Video 13

--- src/main/java/com/example/intern/security/SecurityConfiguration.java
+++ src/main/java/com/example/intern/security/SecurityConfiguration.java
@@ -3,10 +3,13 @@ package com.example.intern.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,12 +17,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
 
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -31,12 +40,16 @@ public class SecurityConfiguration {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(("/createnewuser")).permitAll();
 
                     authorize.anyRequest().authenticated();
 
 
         })
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(
+                        new BasicAuthenticationFilter(authenticationManager(httpSecurity)),
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .build();
 
     }
